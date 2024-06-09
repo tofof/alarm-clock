8ef18afa4f2054641b19c7436f5573a12ec79646
Brian James <github@tofof.com>
2024-06-09 12:21:40 -0500

    spiffs filesystem used

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
 #include <Arduino.h>
+#include "AudioFileSourceSPIFFS.h"
 #include "AudioGeneratorAAC.h"
 #include "AudioOutputI2SNoDAC.h"
 #include "AudioFileSourcePROGMEM.h"
-#include "reveille.h"
 
-AudioFileSourcePROGMEM *in;
+AudioFileSourceSPIFFS *file;
 AudioGeneratorAAC *aac;
 AudioOutputI2SNoDAC *out;
 
 void setup()
 {
   Serial.begin(115200);
+  delay(1000);
+  SPIFFS.begin();
+  file = new AudioFileSourceSPIFFS("/reveille.aac");
 
   audioLogger = &Serial;
-  in = new AudioFileSourcePROGMEM(reveille, sizeof(reveille));
   aac = new AudioGeneratorAAC();
   out = new AudioOutputI2SNoDAC();
 
-  aac->begin(in, out);
+  aac->begin(file, out);
 }
 
 
