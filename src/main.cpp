a702eb39dd2d038c93630573fccc136f4db51689
Brian James <github@tofof.com>
2024-06-09 13:26:32 -0500

    add wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,39 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <WiFiClientSecure.h>
+#include "secrets.h"
+
+// Library for Audio
 #include "AudioFileSourceSPIFFS.h"
 #include "AudioGeneratorAAC.h"
 #include "AudioOutputI2SNoDAC.h"
 #include "AudioFileSourcePROGMEM.h"
 
+// Library for NTP Clock
+#include <NTPClient.h>
+//#include <TimeLib.h>
+
+WiFiUDP ntpUDP;
+
 AudioFileSourceSPIFFS *file;
 AudioGeneratorAAC *aac;
 AudioOutputI2SNoDAC *out;
 
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "id.pool.ntp.org", 25200, 600000);
+unsigned long nextUpdTime;
+int time_between_update; //in minutes int
+
+void setup_wifi();
+
 void setup()
 {
   Serial.begin(115200);
-  delay(1000);
+  setup_wifi();
+  
   SPIFFS.begin();
   file = new AudioFileSourceSPIFFS("/reveille.aac");
 
@@ -22,7 +44,6 @@ void setup()
   aac->begin(file, out);
 }
 
-
 void loop()
 {
   if (aac->isRunning()) {
@@ -31,5 +52,23 @@ void loop()
     Serial.printf("AAC done\n");
     delay(1000);
   }
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  WiFi.hostname("AlarmClock");
+  WiFi.setPhyMode(WIFI_PHY_MODE_11B);
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+  Serial.print("WiFi connected on IP address ");
+  Serial.println(WiFi.localIP());
 }
 
