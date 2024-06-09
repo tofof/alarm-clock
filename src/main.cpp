7c6da9bf2a5bfe2c158fd02b9828b763cc8a2271
Brian James <github@tofof.com>
2024-06-09 11:09:00 -0500

    basic transistor progmem test

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,33 @@
 #include <Arduino.h>
+#include "AudioGeneratorAAC.h"
+#include "AudioOutputI2SNoDAC.h"
+#include "AudioFileSourcePROGMEM.h"
+#include "sampleaac.h"
 
-#define DEBUG 1
-#define LED_PIN D4
+AudioFileSourcePROGMEM *in;
+AudioGeneratorAAC *aac;
+AudioOutputI2SNoDAC *out;
 
-// put function declarations here:
-int myFunction(int, int);
+void setup()
+{
+  Serial.begin(115200);
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  audioLogger = &Serial;
+  in = new AudioFileSourcePROGMEM(sampleaac, sizeof(sampleaac));
+  aac = new AudioGeneratorAAC();
+  out = new AudioOutputI2SNoDAC();
+
+  aac->begin(in, out);
 }
 
-int ledIntensity = 0;
-int step = 1;
 
-void loop() {
-  analogWrite(LED_PIN, ledIntensity);
-  delay(10);
-  ledIntensity += step;
-  if (ledIntensity % 255 == 0)
-    step *= -1;
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  if (aac->isRunning()) {
+    aac->loop();
+  } else {
+    Serial.printf("AAC done\n");
+    delay(1000);
+  }
 }
 
